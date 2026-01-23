dfba8e79b42c4878142c388b182226600b0d6852
Stella Elian <elian_stella@yahoo.com>
2026-01-23 01:13:44 +0100

    Use config file for database credentials

--- DBConnect.php
+++ DBConnect.php
@@ -1,11 +1,18 @@
 <?php
 // fichier sert à se connecter à la base de données 
 // La classe sert uniquement ànse connecter à la base de données 
-class DBConnect{ 
+class DBConnect
+{ 
     public function getPDO():PDO 
     // PDO : outil php qui permet de parler avec la base de données, on y précise le nom de la base, l'adresse du serveur host, le type de charactère, l'identifiant qui est root par defaut et le motde passe qui est vide par défaut 
     {
-        return new PDO ('mysql:dbname=carnet_adresses;host=127.0.0.1;charset=utf8','root','');
+        $config = require 'config.php';
+        return new PDO
+        (
+            'mysql:dbname=' . $config['db_name'] . ';host=' . $config['db_host'] . ';charset=utf8',
+            $config['db_user'],
+            $config['db_pass']
+        );
     }
 
 }
